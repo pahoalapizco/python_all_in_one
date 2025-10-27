46b003e0070f9627c79183c43922e34325711db2
pahoalapizco <paolac.alapizco@gmail.com>
2025-10-27 12:39:47 -0700

    ✨ POO - Getters & Setters

--- POO/book.py
+++ POO/book.py
@@ -1,22 +1,61 @@
-class Book:    
-    def __init__(self, title: str, author: str, isbn: str, is_available: bool = False) -> None:
-        self.title = title
-        self.author = author
-        self.isbn = isbn
-        self.is_available = is_available
+class Book:
+    def __init__(self, title: str, author: str, isbn: str, is_available: bool = True) -> None:
+        self.title: str = title
+        self.author: str = author
+        self.isbn: str = isbn
+        self.is_available: bool = is_available
+        self.__borrowed_times: int = 0
+    
+    def __str__(self) -> str:
+        msj = "This title is" + ("" if self.is_available else " not") + " available"
+        return f"Book: {self.title} \nAuthor: {self.author}. \n{msj}."
 
+    @property
+    def borrow_times(self) -> int:
+        return self.__borrowed_times
+    
+    @borrow_times.setter
+    def borrow_times(self, times: int) -> None:
+        self.__borrowed_times = times
+    
+    def borrow(self) -> None:
+        if self.is_available:
+            self.is_available = False
+            self.borrow_times += 1
+        else:
+            raise ValueError("Sorry this book is not available.")
+    
+    def put_back(self) -> None:
+        if not self.is_available:
+            self.is_available = True
+        else:
+            raise ValueError("Sorry this book has already.")
 
+    def is_popular(self) -> bool:
+        return self.borrow_times > 5
+
+def borrow_book_similator(book: Book, borrow_times: int) -> None:
+    for _ in range(borrow_times):
+        if not book.is_available:
+            book.put_back()        
+        book.borrow()
 
 if __name__ == "__main__":
-    books = {
-        "book1": Book("Harry Potter y la Piedra Filosofal", "JK Rowling", "123456789"),
-        "book2": Book("22/11/63", "Stephen King", "789456123"),
-        "book3": Book("El Problema de los Tres Cuerpos", "Cixin Liu", "456123789", True),
-    }
+    try:
+        books = {
+            "book1": Book("Harry Potter y la Piedra Filosofal", "JK Rowling", "123456789"),
+            "book2": Book("22/11/63", "Stephen King", "789456123"),
+            "book3": Book("El Problema de los Tres Cuerpos", "Cixin Liu", "456123789", False),
+        }
 
-    for (name, book) in books.items():
-        print(f" {name} ".center(15, "="))
-        print(f"Title: {book.title}")
-        print(f"Author: {book.author}")
-        msj = "This title is" + ("" if book.is_available else " not") + " available"
-        print(msj)
+        for (name, book) in books.items():
+            print(book, "\n")
+        
+        # Simular prestamo de libros:
+        borrow_book_similator(books["book1"], 6)
+        borrow_book_similator(books["book3"], 3)
+        
+        print(f"{books["book1"].title} is", ("" if books["book1"].is_popular() else "not") ,"popular.")
+        print(f"{books["book3"].title} is", ("" if books["book3"].is_popular() else "not") ,"popular.")
+    except ValueError as e:
+        print(e)
